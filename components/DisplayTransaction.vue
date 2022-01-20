25257e564c41f3e13f91ef7858fedc2cd5a992bd
philipszdavido <kurtwanger40@gmail.com>
2022-01-20 11:16:14 +0100

    added display transaction

---
+++ components/DisplayTransaction.vue
@@ -0,0 +1,66 @@
+<template>
+  <div
+    class="w-full flex flex-col space-y-4 bg-white rounded-lg p-4 space-y-2 overflow-hidden"
+  >
+    <h2 class="mb-1 text-xl font-bold">
+      View Transaction ({{ transaction['account'] }})
+    </h2>
+    <a-descriptions
+      :column="{ xxl: 4, xl: 3, lg: 3, md: 3, sm: 2, xs: 1 }"
+      layout="vertical"
+      bordered
+    >
+      <a-descriptions-item label="Account" span="1">
+        {{ transaction['account'] }}
+      </a-descriptions-item>
+      <a-descriptions-item label="Description">
+        {{ transaction['description'] }}
+      </a-descriptions-item>
+      <a-descriptions-item label="Category">
+        {{ transaction['category'] }}
+      </a-descriptions-item>
+      <a-descriptions-item label="Reference">
+        {{ transaction['reference'] }}
+      </a-descriptions-item>
+      <a-descriptions-item label="Currency" :span="2">
+        {{ transaction['currency'] }}
+      </a-descriptions-item>
+      <a-descriptions-item label="Amount" :span="3">
+        {{ transaction['amount'] }}
+      </a-descriptions-item>
+      <a-descriptions-item label="Status">
+        {{ transaction['status'] }}
+      </a-descriptions-item>
+      <a-descriptions-item label="Transaction Date">
+        {{ transaction['transactionDate'] }}
+      </a-descriptions-item>
+      <a-descriptions-item label="Created At">
+        {{ transaction['createdAt'] }}
+      </a-descriptions-item>
+      <a-descriptions-item label="Updated At">
+        {{ transaction['updatedAt'] }}
+      </a-descriptions-item>
+    </a-descriptions>
+  </div>
+</template>
+<script>
+import { getTransactionQuery } from './../graphql/query'
+
+export default {
+  data() {
+    return {
+      transaction: {},
+    }
+  },
+  // get transaction in lifecycle hook
+  async created() {
+    const { data } = await this.$apollo.query({
+      query: getTransactionQuery,
+      variables: {
+        transactionId: +this.$route.params.id,
+      },
+    })
+    this.transaction = data.transaction
+  },
+}
+</script>
