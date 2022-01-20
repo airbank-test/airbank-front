b660373ed9c641dba5d3b7fbb25c2921f48ef6fb
philipszdavido <kurtwanger40@gmail.com>
2022-01-20 11:15:48 +0100

    added header

---
+++ components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <header class="flex items-center py-1 px-4 md:px-6 min-h-15 bg-white shadow">
+    <div
+      class="logo-cnt flex items-center inline-flex py-2 px-0.5 -m-0.5 leading-none rounded focus-visible:ring-2 ring-offset-2 transition-all outline-none"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 119 24"
+        fill="currentColor"
+        aria-label="Airbank"
+        width="119"
+        height="20"
+      >
+        <path
+          d="M111.32 14.736l6.87-7.7v-.329h-5.83l-5.379 6.181V.032h-4.656v23.673h4.656v-6.954l5.537 6.954h6.021v-.327zm-28.3 8.977H88v-9.431a3.029 3.029 0 013.078-3.322c1.975 0 2.856 1.171 2.856 3.122v9.63h4.981v-11.19c0-3.736-2.522-6.11-6.283-6.11A5.775 5.775 0 0088 8.332h-.127l-.849-1.625h-4.008zM71.228 20.1c-1.491 0-2.269-.717-2.269-1.7 0-.908.714-1.625 2.3-1.625h3.2v.725c.014 1.4-1.192 2.6-3.231 2.6zM69.967 24a5.789 5.789 0 004.533-1.816h.127l.357 1.529h4.466V12.53c0-3.545-2.784-6.11-7.417-6.11-4.99 0-7.282 2.732-7.282 5.918h4.656c0-1.043.9-1.888 2.467-1.888 1.848 0 2.594.908 2.594 2.087v.908H70.84c-4.664 0-6.8 2.079-6.8 5.233C64.041 21.7 66.144 24 69.967 24zM53.7 19.619c-2.467 0-3.34-2.047-3.34-4.421s.809-4.389 3.34-4.389 3.34 2.015 3.34 4.389-.868 4.421-3.34 4.421zM55.228 24c4.276 0 6.862-3.7 6.862-8.81 0-5.074-2.586-8.778-6.862-8.778a5.408 5.408 0 00-4.506 1.92H50.6v-8.3h-4.987v23.673h4.467l.52-1.625h.127A5.419 5.419 0 0055.228 24zm-22.148-.287h4.657v-8.4c0-2.732 1.523-4.325 3.887-4.325h1.618V6.412h-1.167a4.714 4.714 0 00-4.339 2.764h-.127l-.849-2.469h-3.68zm-8.552 0h4.982V6.707h-4.982zm-.166-19.029h5.307V0h-5.307zM7.941 14.9l2.181-8.85h1.293l2.166 8.85zM14.89.717H6.854L0 23.387v.327h5.22l1.325-4.325h8.417l1.325 4.325h5.482v-.327z"
+        ></path>
+      </svg>
+    </div>
+    <nav
+      class="hidden md:flex flex-1 items-center md:mx-6 lg:mx-16 md:space-x-1 lg:space-x-4 xl:space-x-6"
+    >
+      <a
+        href="/"
+        class="py-2 px-2 lg:px-4 hover:bg-linen rounded focus-visible:ring-2 ring-offset-2 transition-all outline-none"
+      >
+        Transactions
+      </a>
+    </nav>
+  </header>
+</template>
+
+<style>
+.logo-cnt {
+  height: 50px;
+}
+</style>
