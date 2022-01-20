640f223cd1268ebf13128419ea9a6a66c2c6c566
philipszdavido <kurtwanger40@gmail.com>
2022-01-20 11:15:33 +0100

    added welcome display

---
+++ components/WelcomeArea.vue
@@ -0,0 +1,16 @@
+<template>
+  <aside
+    class="flex md:overflow-hidden flex-col space-y-6 md:w-60 lg:w-64 xl:w-68 drop-shadow"
+  >
+    <div class="bg-white rounded-lg p-4">
+      <small class="text-xs text-darksilver">January 2022 </small>
+      <p class="text-xl">Welcome!</p>
+    </div>
+  </aside>
+</template>
+
+<script>
+export default {
+  name: 'WelcomeArea',
+}
+</script>
