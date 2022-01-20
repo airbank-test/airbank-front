cca4874e27039f5455f7cdb42e6aab009e83f961
philipszdavido <kurtwanger40@gmail.com>
2022-01-20 11:16:32 +0100

    added default layout

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <link
+      href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css"
+      rel="stylesheet"
+    />
+
+    <Header />
+    <div class="mb-6 w-full" />
+    <div
+      class="flex md:overflow-hidden overflow-y-auto flex-col md:flex-row flex-1 p-4 md:p-6 space-y-6 md:space-y-0 md:space-x-6 min-h-full md:min-h-0 flex-1"
+    >
+      <WelcomeArea />
+      <Nuxt />
+    </div>
+  </div>
+</template>
